7868b360f1aded809879aace7cfa1c203b63d283
Juha Pitkänen <juhapit@gmail.com>
2024-04-04 20:04:52 +0300

    Updated packages, changed PWA background color and small style fixes to stats

--- frontend/src/app/components/stats-chart/stats-chart.component.ts
+++ frontend/src/app/components/stats-chart/stats-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, Input, OnChanges } from '@angular/core';
+import { Component, ViewChild, Input, OnChanges, OnInit } from '@angular/core';
 import { ChartConfiguration, ChartData, ChartType } from 'chart.js';
 import { BaseChartDirective, NgChartsModule } from 'ng2-charts';
 
@@ -13,20 +13,29 @@ import DataLabelsPlugin from 'chartjs-plugin-datalabels';
   templateUrl: './stats-chart.component.html',
   styleUrl: './stats-chart.component.scss',
 })
-export class StatsChartComponent implements OnChanges {
+export class StatsChartComponent implements OnInit, OnChanges {
   @Input() data!: ChartData<'bar'>;
   @Input() chartType!: StatChartType;
 
   @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+
+  private colors: any = {
+    orange: {
+      default: 'rgba(255, 102, 0, 0.5)',
+      half: 'rgba(255, 102, 0, 0.25)',
+      quarter: 'rgba(255, 102, 0, 0.12)',
+      zero: 'rgba(255, 102, 0, 0)',
+    },
+  };
 
   public barChartOptions: ChartConfiguration['options'] = {
     // We use these empty structures as placeholders for dynamic theming.
     responsive: true,
     elements: {
       bar: {
-        backgroundColor: 'rgba(255, 102, 0, 0.3)',
+        backgroundColor: 'rgba(255, 102, 0, 0.2)',
         borderColor: 'rgba(255, 102, 0, 1)',
-        borderWidth: 2,
+        borderWidth: 1,
         borderRadius: 4,
       },
     },
@@ -51,6 +60,28 @@ export class StatsChartComponent implements OnChanges {
   public barChartPlugins = [DataLabelsPlugin];
 
   constructor() {}
+
+  ngOnInit() {
+    const canvas = <HTMLCanvasElement>document.getElementById('stats-chart');
+    const ctx = canvas.getContext('2d');
+
+    if (ctx) {
+      ctx.canvas.height = 100;
+
+      const gradient = ctx.createLinearGradient(0, 25, 0, 300);
+      gradient.addColorStop(0, this.colors.orange.half);
+      gradient.addColorStop(0.35, this.colors.orange.quarter);
+      gradient.addColorStop(1, this.colors.orange.zero);
+
+      if (
+        this.barChartOptions &&
+        this.barChartOptions.elements &&
+        this.barChartOptions.elements.bar
+      ) {
+        this.barChartOptions.elements.bar.backgroundColor = gradient;
+      }
+    }
+  }
 
   ngOnChanges() {
     if (
